df361322d2d268e1ef2845ebfabf3c627a7fb77b
Jojo Maninang <110759203+UrWebDev@users.noreply.github.com>
2025-01-18 01:08:13 +0800

    Refactor announcement controller to improve functionality and remove department announcement methods

--- src/controllers/announcement.ts
+++ src/controllers/announcement.ts
@@ -1,17 +1,13 @@
 import { Request, Response } from 'express';
 import { prisma } from '../config/database';
 
-export const getAnnouncements = async (req: Request, res: Response) => {
-    const announcements = await prisma.announcement.findFirst();
+const getAnnouncements = async (req: Request, res: Response) => {
+    const announcements = await prisma.announcement.findMany();
 
-    if (announcements) {
-        return res.send(announcements);
-    }
-
-    return res.status(404).send('Announcements not found');
+    return res.status(200).send(announcements);
 };
 
-export const getAnnouncementById = async (req: Request, res: Response) => {
+const getAnnouncementById = async (req: Request, res: Response) => {
     const { id } = req.params;
     const announcement = await prisma.announcement.findUnique({
         where: { id: Number(id) },
@@ -23,7 +19,7 @@ export const getAnnouncementById = async (req: Request, res: Response) => {
     }
 };
 
-export const createAnnouncement = async (req: Request, res: Response) => {
+const createAnnouncement = async (req: Request, res: Response) => {
     const { title, body } = req.body;
 
     // Check if title and body are provided
@@ -35,12 +31,16 @@ export const createAnnouncement = async (req: Request, res: Response) => {
         data: { title, body },
     });
 
-    return res.send(201).send(newAnnouncement);
+    return res.status(201).send(newAnnouncement);
 };
 
-export const updateAnnouncement = async (req: Request, res: Response) => {
+const updateAnnouncement = async (req: Request, res: Response) => {
     const { id } = req.params;
     const { title, body } = req.body;
+
+    if (!Number(id)) {
+        return res.status(400).send('Please provide an id');
+    }
 
     if (!title || !body) {
         return res.status(400).send('Please provide a title and body');
@@ -54,15 +54,15 @@ export const updateAnnouncement = async (req: Request, res: Response) => {
         return res.status(404).send('Announcement not found');
     }
 
-
     const updatedAnnouncement = await prisma.announcement.update({
         where: { id: Number(id) },
         data: { title, body },
     });
+
     return res.send(updatedAnnouncement);
 };
 
-export const deleteAnnouncement = async (req: Request, res: Response) => {
+const deleteAnnouncement = async (req: Request, res: Response) => {
     const { id } = req.params;
 
     const announcement = await prisma.announcement.findUnique({
@@ -79,81 +79,103 @@ export const deleteAnnouncement = async (req: Request, res: Response) => {
     return res.status(204).send("Announcement deleted successfully");
 };
 
-export const getDepartmentAnnouncements = async (req: Request, res: Response) => {
-    const departmentAnnouncements = await prisma.departmentAnnouncement.findMany();
-    if (!departmentAnnouncements.length) {
-        return res.status(404).send('Department announcements not found');
-    }
+// const getDepartmentAnnouncements = async (req: Request, res: Response) => {
+//     const departmentAnnouncements = await prisma.departmentAnnouncement.findMany();
+//     if (!departmentAnnouncements.length) {
+//         return res.status(404).send('Department announcements not found');
+//     }
 
-    return res.send(departmentAnnouncements);
+//     return res.send(departmentAnnouncements);
+// };
 
+// const getDepartmentAnnouncementById = async (req: Request, res: Response) => {
+//     const { id } = req.params;
+
+//     const departmentAnnouncement = await prisma.departmentAnnouncement.findUnique({
+//         where: { id: Number(id) },
+//     });
+
+//     if (departmentAnnouncement) {
+//         return res.send(departmentAnnouncement);
+//     } else {
+//         return res.status(404).send('Department announcement not found');
+//     }
+// }
+
+// const createDepartmentAnnouncement = async (req: Request, res: Response) => {
+//     const { title, body, departmentId } = req.body;
+//     if (!Number(departmentId)) {
+//         return res.status(400).send('Please provide a departmentId');
+//     }
+
+//     if (!title || !body) {
+//         return res.status(400).send('Please provide a title and body');
+//     }
+
+//     const newDepartmentAnnouncement = await prisma.departmentAnnouncement.create({
+//         data: { title, body, Department: { connect: { id: Number(departmentId) } } },
+//     });
+
+//     return res.status(201).send(newDepartmentAnnouncement);
+// };
+
+// const updateDepartmentAnnouncement = async (req: Request, res: Response) => {
+//     const { id } = req.params;
+//     const { title, body, departmentId } = req.body;
+
+//     if (!Number(id)) {
+//         return res.status(400).send('Please provide an id');
+//     }
+
+//     if (!title || !body || !Number(id)) {
+//         return res.status(400).send('Please provide a title and body');
+//     }
+
+//     const departmentAnnouncement = await prisma.departmentAnnouncement.findUnique({
+//         where: { id: Number(id) },
+//     });
+
+//     if (!departmentAnnouncement) {
+//         return res.status(404).send('Department announcement not found');
+//     }
+
+//     const updatedDepartmentAnnouncement = await prisma.departmentAnnouncement.update({
+//         where: { id: Number(id) },
+//         data: { title, body, departmentId: Number(departmentId) },
+//     });
+
+//     return res.send(updatedDepartmentAnnouncement);
+// };
+
+// const deleteDepartmentAnnouncement = async (req: Request, res: Response) => {
+//     const { id } = req.params;
+//     if (!Number(id)) {
+//         return res.status(400).send('Please provide an id');
+//     }
+
+//     const departmentAnnouncement = await prisma.departmentAnnouncement.findUnique({
+//         where: { id: Number(id) },
+//     });
+
+//     if (!departmentAnnouncement) {
+//         return res.status(404).send('Department announcement not found');
+//     }
+
+//     await prisma.departmentAnnouncement.delete({
+//         where: { id: Number(id) },
+//     });
+//     return res.status(204).send("Department announcement deleted successfully");
+// }
+
+export default {
+    getAnnouncements,
+    getAnnouncementById,
+    createAnnouncement,
+    updateAnnouncement,
+    deleteAnnouncement,
+    // getDepartmentAnnouncements,
+    // getDepartmentAnnouncementById,
+    // createDepartmentAnnouncement,
+    // updateDepartmentAnnouncement,
+    // deleteDepartmentAnnouncement,
 };
-
-export const getDepartmentAnnouncementById = async (req: Request, res: Response) => {
-    const { id } = req.params;
-
-    const departmentAnnouncement = await prisma.departmentAnnouncement.findUnique({
-        where: { id: Number(id) },
-    });
-    if (departmentAnnouncement) {
-        res.send(departmentAnnouncement);
-    } else {
-        res.status(404).send('Department announcement not found');
-    }
-}
-
-export const createDepartmentAnnouncement = async (req: Request, res: Response) => {
-    const { title, body, departmentId } = req.body;
-    if (!title || !body || !departmentId) {
-        return res.status(400).send('Please provide a title, body and departmentId');
-    }
-
-    const newDepartmentAnnouncement = await prisma.departmentAnnouncement.create({
-        data: { title, body, departmentId },
-    });
-
-    return res.status(201).send(newDepartmentAnnouncement);
-};
-
-export const updateDepartmentAnnouncement = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { title, body, departmentId } = req.body;
-    if (!title || !body || !Number(id) || !departmentId) {
-        return res.status(400).send('Please provide a title, body and departmentId');
-    }
-
-    const departmentAnnouncement = await prisma.departmentAnnouncement.findUnique({
-        where: { id: Number(id) },
-    });
-
-    if (!departmentAnnouncement) {
-        return res.status(404).send('Department announcement not found');
-    }
-
-    const updatedDepartmentAnnouncement = await prisma.departmentAnnouncement.update({
-        where: { id: Number(id) },
-        data: { title, body, departmentId },
-    });
-
-    return res.send(updatedDepartmentAnnouncement);
-};
-
-export const deleteDepartmentAnnouncement = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    if (!Number(id)) {
-        return res.status(400).send('Please provide an id');
-    }
-
-    const departmentAnnouncement = await prisma.departmentAnnouncement.findUnique({
-        where: { id: Number(id) },
-    });
-
-    if (!departmentAnnouncement) {
-        return res.status(404).send('Department announcement not found');
-    }
-
-    await prisma.departmentAnnouncement.delete({
-        where: { id: Number(id) },
-    });
-    return res.status(204).send("Department announcement deleted successfully");
-}
