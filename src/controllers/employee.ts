805cd5dc5f5d26b0727fb2b63cc8f140166e8e7a
Ervin Pangilinan <pangilinanervin22@gmail.com>
2025-02-18 01:03:35 +0800

    feat: enhance employee management with role and department filters, and add email validation

--- src/controllers/employee.ts
+++ src/controllers/employee.ts
@@ -1,13 +1,23 @@
 import bcrypt from 'bcryptjs';
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import { prisma } from "../config/database";
 import { validateCreateOneEmployee, validateEmployee, validateUpdateEmployee } from "../validate/employee.validation";
 import { customThrowError } from '../middlewares/errorHandler';
+import { io } from '../index';
 
 async function getAllEmployees(req: Request, res: Response) {
+    const departmentId = req.query.department ? Number(req.query.department) : undefined;
+    const role = req.query.role ? String(req.query.role).toUpperCase() : undefined;
+
     const allEmployees = await prisma.employee.findMany({
+        where: {
+            departmentId,
+            role,
+
+        },
         select: {
             id: true,
+            email: true,
             username: true,
             firstName: true,
             lastName: true,
@@ -21,6 +31,8 @@ async function getAllEmployees(req: Request, res: Response) {
             updatedAt: "desc",
         },
     });
+
+    io.emit("update-employee")
 
     res.status(200).send(allEmployees);
 }
@@ -47,6 +59,12 @@ async function getAllTeamLeaders(req: Request, res: Response) {
 }
 
 async function getAllOnlyEmployee(req: Request, res: Response) {
+    const { role } = req.query;
+
+    if (role && !["EMPLOYEE", "TEAM_LEADER", "ADMIN"].includes(String(role))) {
+        return customThrowError(400, "Invalid role");
+    }
+
     const allTeamMembers = await prisma.employee.findMany({
         where: {
             accessLevel: "EMPLOYEE",
@@ -66,6 +84,8 @@ async function getAllOnlyEmployee(req: Request, res: Response) {
 
     res.status(200).send(allTeamMembers);
 }
+
+
 
 async function getAllAdmin(req: Request, res: Response) {
     const allAdmins = await prisma.employee.findMany({
@@ -116,7 +136,21 @@ async function getEmployeeById(req: Request, res: Response) {
 }
 
 const createEmployee = async (req: Request, res: Response) => {
+
     validateCreateOneEmployee(req.body);
+
+    const body = {
+        email: req.body.email,
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        middleName: req.body.middleName,
+        accessLevel: req.body.accessLevel,
+        isActive: req.body.isActive,
+        passwordCredentials: req.body.passwordCredentials,
+        role: req.body.role,
+    }
+
 
     const existingEmployeeUsername = await prisma.employee.findFirst({
         where: {
@@ -128,19 +162,30 @@ const createEmployee = async (req: Request, res: Response) => {
         customThrowError(400, "Username already exists");
     }
 
+    const existingEmployeeEmail = await prisma.employee.findFirst({
+        where: {
+            email: req.body.email,
+        },
+    });
+
+    if (existingEmployeeEmail) {
+        customThrowError(400, "Email already used");
+    }
+
     const hashedPassword = await bcrypt.hash(req.body.passwordCredentials, 10);
 
     // Create employee
     await prisma.employee.create({
         data: {
-            username: req.body.username.trim(),
-            firstName: req.body.firstName.trim(),
-            lastName: req.body.lastName.trim(),
-            middleName: req.body.middleName.trim(),
-            accessLevel: req.body.accessLevel,
-            isActive: req.body.isActive,
+            email: body.email,
+            username: body.username,
+            firstName: body.firstName,
+            lastName: body.lastName,
+            middleName: body.middleName,
+            accessLevel: body.accessLevel,
+            isActive: body.isActive,
             passwordCredentials: hashedPassword,
-            role: req.body.role,
+            role: body.role,
         },
     });
 
@@ -152,14 +197,15 @@ const updateEmployee = async (req: Request, res: Response) => {
 
     validateUpdateEmployee(req.body);
     const body = {
-        username: req.body.username ? req.body.username.trim() : undefined,
-        firstName: req.body.firstName ? req.body.firstName.trim() : undefined,
-        lastName: req.body.lastName ? req.body.lastName.trim() : undefined,
-        middleName: req.body.middleName ? req.body.middleName.trim() : undefined,
+        email: req.body.email ? req.body.email : undefined,
+        username: req.body.username ? req.body.username : undefined,
+        firstName: req.body.firstName ? req.body.firstName : undefined,
+        lastName: req.body.lastName ? req.body.lastName : undefined,
+        middleName: req.body.middleName ? req.body.middleName : undefined,
         accessLevel: req.body.accessLevel,
         isActive: req.body.isActive,
         role: req.body.role,
-        password: req.body.password ? req.body.password.trim() : undefined,
+        password: req.body.password ? req.body.password : undefined,
     }
 
     const existingEmployee = await prisma.employee.findUnique({
@@ -174,6 +220,7 @@ const updateEmployee = async (req: Request, res: Response) => {
     await prisma.employee.update({
         where: { id: Number(req.params.id) },
         data: {
+            email: body.email ?? existingEmployee.email,
             username: body.username ?? existingEmployee.username,
             firstName: body.firstName ?? existingEmployee.firstName,
             lastName: body.lastName ?? existingEmployee.lastName,
