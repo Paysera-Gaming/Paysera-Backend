c21c11368eb204a102d924ac1e6863e465e54ba3
Ervin Pangilinan <pangilinanervin22@gmail.com>
2025-02-19 00:41:39 +0800

    feat: refactor Socket.IO server setup and enhance CORS configuration

--- src/index.ts
+++ src/index.ts
@@ -26,25 +26,6 @@ import { Server } from "socket.io";
 
 const app = express();
 const port = configEnv.PORT || 3000;
-// Export the server for testing
-const server = http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: [configEnv.ORIGIN, 'https://x3lkcvjr-5173.asse.devtunnels.ms', 'https://x3lkcvjr-5173.asse.devtunnels.ms'],  // Allow your React app to connect
-        methods: ['GET', 'POST'],
-    },
-})
-
-// Socket.IO connection
-io.on('connection', (socket) => {
-    console.log('New client connected:', socket.id);
-
-    socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
-    });
-});
-
 
 if (process.env.NODE_ENV === 'production') {
     app.set('trust proxy', 1); // Trust first proxy in production
@@ -100,6 +81,25 @@ app.use(globalErrorHandler);
 // 404 Error Middleware`
 app.use((req, res, next) => {
     res.status(404).send({ error: 'Route Not Found' });
+});
+
+// Export the server for testing
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: [configEnv.ORIGIN, 'https://x3lkcvjr-5173.asse.devtunnels.ms', 'https://x3lkcvjr-5173.asse.devtunnels.ms'],  // Allow your React app to connect
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow your React app to make requests
+    },
+})
+
+// Socket.IO connection
+io.on('connection', (socket) => {
+    console.log('New client connected:', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+    });
 });
 
 if (configEnv.NODE_ENV !== 'test') {
