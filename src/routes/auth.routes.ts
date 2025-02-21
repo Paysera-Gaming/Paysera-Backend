35174fa1e0d45d9e3111818e2674ceaa3ebd7ada
Ervin Pangilinan <pangilinanervin22@gmail.com>
2025-02-22 01:04:57 +0800

    feat: add password recovery routes for sending reset email and resetting password

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,14 +1,17 @@
 import express from 'express';
-import { login, logout, getUserInfo } from '../controllers/auth';
+import { login, logout, getUserInfo, sendForgetPasswordEmail, resetPassword } from '../controllers/auth';
 import { asyncHandler } from '../middlewares/errorHandler';
 import { adminMiddleware, teamLeaderMiddleware, regularMiddleware } from '../middlewares';
 
 const routerAuth = express.Router();
 
-
 routerAuth.post('/login', asyncHandler(login)); //user login
 routerAuth.post('/logout', asyncHandler(logout)); //user logout
 routerAuth.get('/info', asyncHandler(getUserInfo)); //user logout
+routerAuth.post('/forget-password', asyncHandler(sendForgetPasswordEmail)); //user logout
+routerAuth.post('/reset-password/:id', asyncHandler(resetPassword)); //user logout
+
+
 
 routerAuth.get('/protected', regularMiddleware, (req, res) => {
     res.send('success');
