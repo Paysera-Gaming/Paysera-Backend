702ca97f785a0d9936c5c968dde1550aa09dae11
Ervin Pangilinan <pangilinanervin22@gmail.com>
2025-02-22 01:04:41 +0800

    refactor: update test suite to use app instance instead of server for department routes

--- src/test/department.test.ts
+++ src/test/department.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import { server } from '..';
+import app from '..';
 import { Department, Employee } from '@prisma/client';
 
 describe('Department Routes Test', () => {
@@ -8,36 +8,37 @@ describe('Department Routes Test', () => {
 
     describe('GET Department', () => {
         it('should return a list of departments', async () => {
-            const response = await request(server).get('/api/department').expect(200);
+            const response = await request(app).get('/api/department').expect(200);
             expect(response.body.length).toBeGreaterThanOrEqual(1);
             departmentList = response.body;
         });
 
         it('should return a single department', async () => {
-            const response = await request(server).get(`/api/department/${departmentList[0].id}`).expect(200);
+            const response = await request(app).get(`/api/department/${departmentList[0].id}`).expect(200);
             expect(response.body).toHaveProperty('id');
             expect(response.body.id).toBe(departmentList[0].id);
             expect(response.body.name).toBe(departmentList[0].name);
         });
 
         it('should return a 404', async () => {
-            await request(server).get('/api/department/0').expect(404);
+            await request(app).get('/api/department/-1').expect(404);
         });
     });
 
     describe('POST Department', () => {
         it('should create a new department', async () => {
-
-
             const body = {
                 name: 'Department POST',
                 leaderId: departmentList[0].leaderId
             };
 
-            const response = await request(server)
+            const response = await request(app)
                 .post('/api/department')
                 .send(body)
-                .expect(201);
+
+            console.log(response.body, 'response.body');
+            expect(response.status).toBe(201);
+
         });
 
         it('should return 404 for non-existent leader', async () => {
@@ -46,7 +47,7 @@ describe('Department Routes Test', () => {
                 leaderId: 100
             };
 
-            await request(server)
+            await request(app)
                 .post('/api/department')
                 .send(newDepartment)
                 .expect(404);
@@ -55,7 +56,7 @@ describe('Department Routes Test', () => {
 
     describe('PUT Department', () => {
         it('should update a department', async () => {
-            const response = await request(server)
+            const response = await request(app)
                 .put(`/api/department/${departmentList[0].id}`)
                 .send({ name: 'Department PUT', })
                 .expect(200);
@@ -66,16 +67,16 @@ describe('Department Routes Test', () => {
         let departmentId: Number;
 
         it('should return a department leader', async () => {
-            const deptResponse = await request(server).get(`/api/department/${departmentList[0].id}`).expect(200);
+            const deptResponse = await request(app).get(`/api/department/${departmentList[0].id}`).expect(200);
             departmentId = deptResponse.body.id;
             expect(deptResponse.body).toHaveProperty('leaderId');
 
-            await request(server).get('/api/employee/team-leader').expect(200);
+            await request(app).get('/api/employee/team-leader').expect(200);
         });
 
         it('should assign a leader to a department', async () => {
-            const getEmployeeResponse = await request(server).get('/api/employee/team-leader').expect(200);
-            await request(server)
+            const getEmployeeResponse = await request(app).get('/api/employee/team-leader').expect(200);
+            await request(app)
                 .put(`/api/department/${departmentId}/leader`)
                 .send({
                     departmentId: departmentId,
@@ -83,18 +84,18 @@ describe('Department Routes Test', () => {
                 }).expect(200);
 
             // Check if leader is assigned to department
-            const departmentResponse = await request(server).get(`/api/department/${departmentId}`).expect(200);
+            const departmentResponse = await request(app).get(`/api/department/${departmentId}`).expect(200);
             expect(departmentResponse.body.leaderId).toBe(getEmployeeResponse.body[0].id);
 
             // Check if employee is assigned to department
-            const employeeResponse = await request(server).get(`/api/employee/${getEmployeeResponse.body[0].id}`).expect(200);
+            const employeeResponse = await request(app).get(`/api/employee/${getEmployeeResponse.body[0].id}`).expect(200);
             expect(employeeResponse.body.departmentId).toBe(departmentId);
         });
 
         it('should return 400 error assign a employee as leader to a department', async () => {
-            const getEmployeeResponse = await request(server).get('/api/employee/only-employee').expect(200);
+            const getEmployeeResponse = await request(app).get('/api/employee/only-employee').expect(200);
 
-            await request(server)
+            await request(app)
                 .put(`/api/department/${departmentId}/leader`)
                 .send({
                     departmentId: departmentId,
@@ -103,20 +104,20 @@ describe('Department Routes Test', () => {
         });
 
         it('should remove a leader from a department', async () => {
-            const currentDepartment = await request(server).get(`/api/department/${departmentId}`).expect(200);
+            const currentDepartment = await request(app).get(`/api/department/${departmentId}`).expect(200);
 
-            await request(server)
+            await request(app)
                 .delete(`/api/department/${departmentId}/leader`)
                 .send({
                     leaderId: currentDepartment.body.leaderId,
                 }).expect(200);
 
             // Check if leader is removed from department
-            const departmentResponse = await request(server).get(`/api/department/${currentDepartment.body.id}`).expect(200);
+            const departmentResponse = await request(app).get(`/api/department/${currentDepartment.body.id}`).expect(200);
             expect(departmentResponse.body.leaderId).toBe(null);
 
             // Check if employee is removed from department
-            const employeeResponse = await request(server).get(`/api/employee/${currentDepartment.body.leaderId}`).expect(200);
+            const employeeResponse = await request(app).get(`/api/employee/${currentDepartment.body.leaderId}`).expect(200);
             expect(employeeResponse.body.departmentId).toBe(null);
         });
     });
@@ -125,17 +126,17 @@ describe('Department Routes Test', () => {
         let curEmployee: Employee;
 
         it('should return all employee', async () => {
-            const response = await request(server)
+            const response = await request(app)
                 .get(`/api/department/${departmentList[0].id}/employee`).expect(200);
 
             expect(response.body).toBeInstanceOf(Array);
         });
 
         it('should assign employee to department ', async () => {
-            const getEmployeeResponse = await request(server).get('/api/employee/only-employee').expect(200);
+            const getEmployeeResponse = await request(app).get('/api/employee/only-employee').expect(200);
             curEmployee = getEmployeeResponse.body[0];
 
-            await request(server)
+            await request(app)
                 .put(`/api/department/${departmentList[0].id}/employee`)
                 .send({
                     departmentId: departmentList[0].id,
@@ -143,7 +144,7 @@ describe('Department Routes Test', () => {
                     role: 'staff'
                 }).expect(200);
 
-            const deptEmployee = await request(server).get(`/api/department/${departmentList[0].id}/employee`).expect(200);
+            const deptEmployee = await request(app).get(`/api/department/${departmentList[0].id}/employee`).expect(200);
 
             // Check if employee is assigned to 
             expect(deptEmployee.body).toContainEqual(expect.objectContaining({
@@ -154,14 +155,14 @@ describe('Department Routes Test', () => {
 
         it('should remove employee from department', async () => {
 
-            await request(server)
+            await request(app)
                 .delete(`/api/department/${curEmployee.departmentId}/employee`)
                 .send({
                     departmentId: curEmployee.departmentId,
                     employeeId: curEmployee.id,
                 }).expect(200);
 
-            const employeeResponse = await request(server).get(`/api/employee/${curEmployee.id}`).expect(200);
+            const employeeResponse = await request(app).get(`/api/employee/${curEmployee.id}`).expect(200);
             expect(employeeResponse.body.departmentId).toBe(null);
         });
     });
@@ -169,16 +170,18 @@ describe('Department Routes Test', () => {
     describe('DELETE Department', () => {
         it('should delete a department', async () => {
             departmentIdDelete = departmentList[0].id;
-            const response = await request(server).get(`/api/department/${departmentIdDelete}`).expect(200);
+            const response = await request(app).get(`/api/department/${departmentIdDelete}`).expect(200);
 
-            await request(server)
+            await request(app)
                 .delete(`/api/department/${response.body.id}`).expect(200);
 
-            await request(server).get(`/api/department/${response.body.id}`).expect(404);
+            await request(app).get(`/api/department/${response.body.id}`).expect(404);
         });
 
         it('should return a 404', async () => {
-            await request(server).delete('/api/department/0').expect(404);
+            const res = await request(app).delete('/api/department/0').expect(404);
+
+            console.log(res.body, 'res.body 404');
         });
     });
 });
