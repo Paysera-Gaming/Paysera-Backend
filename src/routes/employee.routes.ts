fc025bd6bf2314b63ecc1cba9fcb9e75c0e8dd1b
Ervin Pangilinan <pangilinanervin22@gmail.com>
2025-03-01 02:10:51 +0800

    feat: reorganize employee routes for improved structure and readability

--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -5,18 +5,19 @@ import { teamLeaderMiddleware } from "../middlewares";
 
 const routerEmployee = express.Router();
 
-routerEmployee.route("/team-leader").get(asyncHandler(EmployeeController.getAllTeamLeaders));
-routerEmployee.route("/only-employee").get(asyncHandler(EmployeeController.getAllOnlyEmployee));
-routerEmployee.route("/admin").get(asyncHandler(EmployeeController.getAllAdmin));
-
 routerEmployee
     .route("/")
     .get(asyncHandler(EmployeeController.getAllEmployees))
     .post(teamLeaderMiddleware, asyncHandler(EmployeeController.createEmployee));
+
 routerEmployee
     .route("/:id")
     .get(asyncHandler(EmployeeController.getEmployeeById))
     .delete(teamLeaderMiddleware, asyncHandler(EmployeeController.deleteEmployeeById))
     .put(teamLeaderMiddleware, asyncHandler(EmployeeController.updateEmployee));
+
+routerEmployee.route("/team-leader").get(asyncHandler(EmployeeController.getAllTeamLeaders));
+routerEmployee.route("/only-employee").get(asyncHandler(EmployeeController.getAllOnlyEmployee));
+routerEmployee.route("/admin").get(asyncHandler(EmployeeController.getAllAdmin));
 
 export default routerEmployee;
